5cf9dec409a882e97654a229be98b8a444f0150a
nbourre <nbourre@hotmail.com>
2019-08-28 10:08:05 -0400

    Ajout des projets Window et App

---
+++ wpf_demo_app/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace wpf_demo_app
+{
+    /// <summary>
+    /// Logique d'interaction pour App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            MainWindow wnd = new MainWindow();
+            wnd.Title = "Démarrage en code-behind";
+
+            if (e.Args.Length > 0)
+            {
+                // Gérer les paramètres ici
+                wnd.Message = e.Args[0];
+            }
+            
+            wnd.Show();
+        }
+    }
+}
---
+++ wpf_demo_app/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows;
+
+namespace wpf_demo_app
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string _message;
+
+        public string Message {
+            get
+            {
+                return _message;
+            }
+
+            set {
+                _message = value;
+                this.txBkMsg.Text = _message;
+            }
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+            
+    
+    }
+}
